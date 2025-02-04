1c392b7b6b5d5483c3505601a49b19e9cb3f8bb8
burningfiremoon <charleszhang4@gmail.com>
2025-02-03 17:18:17 -0800

    Transformers not installed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import config
-import imaplib, email, regex as re, codecs, string
+import imaplib, email
+from transformers import pipeline, AutoTokenizer
+# not yet used
+import regex as re, codecs, string
 
 # Imap4_SSL class instance
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -21,16 +24,38 @@ imap.login(USER, PASSWORD)
 imap.select('"INBOX"')
 status, messages = imap.search(None, 'UNSEEN')
 
-# (RFC822) Format
-
+# email fetching
 for num in messages[0].split()[:10][::-1]:
-    # Fetch
+    # Fetch (RFC822) Format
     result, msg = imap.fetch(num, "(RFC822)")
 
     if result == 'OK' and msg is not None:
         message = email.message_from_bytes(msg[0][1])
         # message contains header info
         # sender, date, subject, body, attachments and etc.
+
+        # Multipart email check:
+        if message.is_multipart():
+            for part in message.walk():
+                contentType = part.get_content_type()
+                # to ignore attachments
+                contentDisposition = str(part.get("Contnent-Disposition"))
+
+                if contentType == "text/plain" and "attachment" not in contentDisposition:
+                    # if there's a text/plain part
+                    content = part.get_payload(decode=True)
+                    
+                    if content: # None check
+                        body = content.decode("utf-8")
+                        break
+        else:
+            # Non-multipart email
+            content = message.get_payload(decode=True)
+            if content: # None check
+                body = content.decode("utf-8")
+    
+        if body is None:
+            body = "(No text content found)"
 
         decodedSubject = email.header.decode_header(message['Subject'])
         subject = decodedSubject[0][0]
@@ -42,5 +67,8 @@ for num in messages[0].split()[:10][::-1]:
         print("Subject:", subject)
         print("From:", message["From"])
         print("Date:", message["Date"])
+        print("Body:", body)
     else:
         print(f"Skipped {num}, invalid fetch")
+
+
